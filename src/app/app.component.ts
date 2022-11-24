2777f5dcdbc62a76d8173e3d91e132a8b4fe0e17
Gabriele Panico <gabriele.panico@gmail.com>
2022-11-24 11:29:45 +0100

    changed: dynamic load segment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -335,6 +335,7 @@ export class AppComponent implements OnInit {
 
 
     this.initializeApp('oninit');
+    this.initSegment()
     this.listenToPostMsgs();
   }
 
@@ -1266,6 +1267,19 @@ export class AppComponent implements OnInit {
     } catch (err) {
       this.logger.error('Get local storage dshbrd----foregroundcount ', err)
     }
+  }
+
+  private initSegment(){
+    if(!isDevMode()){
+      var head  = document.getElementsByTagName('head')[0];
+      var script  = document.createElement('script');
+      script.type="text/javascript";
+      script.text =`!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="qaTU0wG6VH6xPAhOzD2kneI70Txg1fTB";;analytics.SNIPPET_VERSION="4.15.3";
+                    analytics.load("qaTU0wG6VH6xPAhOzD2kneI70Txg1fTB");
+                    analytics.page();
+                    }}();`
+      head.appendChild(script);
+    } 
   }
 
   private segmentSignIn(){
